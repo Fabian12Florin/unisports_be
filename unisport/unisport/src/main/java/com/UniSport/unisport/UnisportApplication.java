8279a903dad86d98f186b7dc9a43260ba16c9088
Fabian12Florin <fabian.dogaru@student.upt.ro>
2023-12-29 13:03:42 +0200

    Project initialized

---
+++ unisport/unisport/src/main/java/com/UniSport/unisport/UnisportApplication.java
@@ -0,0 +1,13 @@
+package com.UniSport.unisport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnisportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnisportApplication.class, args);
+	}
+
+}
---
+++ unisport/unisport/src/test/java/com/UniSport/unisport/UnisportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UniSport.unisport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnisportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
